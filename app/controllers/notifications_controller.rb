36477e5b52551b68edaf27a0229c418d6dc68bb3
pam- <pam.assogba@gmail.com>
2014-09-03 19:05:38 -0400

    messages are sending!

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,17 +6,13 @@ class NotificationsController < ApplicationController
 
   def create
     @notification = Notification.new(notification_params)
-
-    # Do some stuff that checks whether the Notification has
-    # the info you need, then sends it! Otherwise, display errors
-    # to the user!
-
-    redirect_to new_notification_path
+    @notification.text
+    redirect_to new_notification_path 
   end
 
   private
   def notification_params
-    params.require(:notification).permit(:to_number)
+    params.permit(:to_number)
   end
 
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,9 +3,10 @@ require 'twilio-ruby'
 class Notification
   attr_accessor :to_number
 
-  def initialize
+  def initialize(options={})
+    self.to_number = options[:to_number]
     account_sid = "ACda60e158299d5121bed6fb400148e653"
-    auth_token = "c43b023d95002071e4742b2428f84622"
+    auth_token =  "c43b023d95002071e4742b2428f84622"
     @client = Twilio::REST::Client.new(account_sid, auth_token)
 
   end
@@ -17,7 +18,8 @@ class Notification
 
   def random_compliment
     # Generates a random compliment to send to the textee
-    
+    compliments = ['Your code is so dry it makes me thuuursty!', 'Your so awesome!', 'Hey girl, wanna clone my repo?', 'GIT PUSH IT!!', 'No limit, you are just like an infinite loop baybay.' ]
+    compliments.sample
   end
 
 end
