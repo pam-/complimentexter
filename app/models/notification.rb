1963cbddaf2cccae33a98145f71da13abc97ecc8
pam- <pam.assogba@gmail.com>
2014-09-03 17:44:26 -0400

    start

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,17 +4,20 @@ class Notification
   attr_accessor :to_number
 
   def initialize
-    account_sid = "YOUR_ACCOUNT_SID"
-    auth_token = "YOUR_AUTH_TOKEN"
+    account_sid = "ACda60e158299d5121bed6fb400148e653"
+    auth_token = "c43b023d95002071e4742b2428f84622"
     @client = Twilio::REST::Client.new(account_sid, auth_token)
+
   end
 
   def text
     # Sends a text to your phone number
+    message = @client.messages.create(body: random_compliment, to: '+12406912665', from: '+14847023658')
   end
 
   def random_compliment
     # Generates a random compliment to send to the textee
+    
   end
 
 end
---
+++ credentials.rb
@@ -0,0 +1,8 @@
+class Credentials
+  def self.credentials
+    {
+      :twilio_account_sid => "ACda60e158299d5121bed6fb400148e653",
+      :twilio_account_token => "c43b023d95002071e4742b2428f84622"
+    }
+  end
+end
